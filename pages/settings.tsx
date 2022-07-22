eedc3cc06c2a6ad85dc39b289fa9d3614fd28dc8
mustafa ışık <mustafaaii@hotmail.com>
2022-07-23 00:08:45 +0300

    update
    update

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -2,19 +2,26 @@ import axios from "axios";
 import { NextPage } from "next";
 import { useEffect, useState } from "react";
 import Input from "../element/input";
-import Table from "../element/table";
 import Header from "../module/header/header";
 import Sidebar from "../module/sidebar/sidebar";
-
+import Swal from 'sweetalert2'
 type DataBaseList = {
     data: [],
     status: boolean,
     totalItems: number
 }
-
-
 const Settings: NextPage = () => {
 
+
+    //#region GLOBAL
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+            confirmButton: 'btn btn-success text-white mr-2 ml-2 rounded-none',
+            cancelButton: 'btn btn-danger text-white mr-2 ml-2 rounded-none'
+        },
+        buttonsStyling: false
+    })
+    //#endregion
 
     //#region  DATABASE LIST
     const [DataBaseList, SetDataBaseList] = useState<DataBaseList>({
@@ -25,22 +32,59 @@ const Settings: NextPage = () => {
     const List = async () => {
         const { data } = await axios.post('https://localhost/api/database/get_database.php', { op: "database_list", });
         SetDataBaseList(data);
-
     }
     //#endregion
 
+    const HandlerDeleteDatabase = async (e: any) => {
+        swalWithBootstrapButtons.fire({
+            title: 'Are you sure?',
+            html: "Are you sure you want to delete database named <b>" + e + "</b> ?",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Yes, delete it!',
+            cancelButtonText: 'No, cancel!',
+            reverseButtons: true
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axios.post('https://localhost/api/database/delete_database.php', { db_name: e, }).then(() => { List(); });
+                swalWithBootstrapButtons.fire('Deleted!', 'Database Deleted.', 'success')
 
-    console.log(DataBaseList)
-
-    const HandlerDeleteDatabase = (e: any) => {
-        alert(e)
+            }
+        })
+    }
+    const HandlerEmptyDatabase = () => {
+        swalWithBootstrapButtons.fire({
+            html: "This Database Cannot Be Deleted or you do not have permission",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Okay, I got it!',
+            reverseButtons: true
+        })
+    }
+    const [database, setdatabasename] = useState('');
+    function hasWhiteSpace(s: any) {
+        return /\s/g.test(s);
     }
 
-
-    const [database, setdatabasename] = useState('');
     const FormSubmit = (event: any) => {
         event.preventDefault();
-        axios.post('https://localhost/api/database/create_database.php', { database: database, }).then(({ data }) => { List(); });
+        console.log(hasWhiteSpace(database))
+        if (hasWhiteSpace(database) === false)
+        {
+            axios.post('https://localhost/api/database/create_database.php', { database: database, }).then(() => { List(); });
+        }
+        else
+        {
+            swalWithBootstrapButtons.fire({
+                html: "No Spaces Allowed in Database Names",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Okay, I got it!',
+                reverseButtons: true
+            })
+        }
+
+
     }
     const Tabsetting = {
         active: "bg-primary text-white",
@@ -108,9 +152,6 @@ const Settings: NextPage = () => {
         List();
 
     }, [])
-
-
-
     //#endregion
 
 
@@ -142,29 +183,31 @@ const Settings: NextPage = () => {
                         </div>
                         <div key={`tab${202290}`} id={`tab${202290}`} className={`px-5 sm:px-20 mt-10 pt-10  border-slate-200/60 dark:border-darkmode-400`} style={{ display: `${0 === Tabsetting.opener ? "block" : "none"}` }}>
                             <div className="grid grid-cols-12 gap-6">
-                                <div className="col-span-12 2xl:col-span-6">
+
+
+                                <div className="col-span-12 2xl:col-span-12">
                                     <form onSubmit={FormSubmit} method="post">
-                                        <div className="font-medium text-base">Create Database 1</div>
-                                        <div className="grid grid-cols-12 gap-4 gap-y-5 mt-5">
-                                            <div className="intro-y col-span-12 sm:col-span-12">
-                                                <label htmlFor="input-wizard-1" className="form-label">Database Name</label>
-                                                <input id="database" name="database" type="text" value={database} className="form-control" onChange={e => { setdatabasename(e.target.value) }} placeholder="Database Name" />
+                                        <div className="font-medium text-base">Create New Database</div>
+                                        <div className="grid grid-cols-12 gap-6 gap-y-5 mt-5">
+                                            <div className="col-span-12 2xl:col-span-10">
+                                                <div className="intro-y">
+                                                    <input id="database" name="database" type="text" value={database} className="form-control" onChange={e => { setdatabasename(e.target.value) }} placeholder="Database Name" />
+                                                </div>
                                             </div>
-                                            <div className="intro-y col-span-12 sm:col-span-12">
-                                                <label htmlFor="input-wizard-1" className="form-label">Database Name</label>
-                                                <input id="database" name="database" type="text" value={database} className="form-control" onChange={e => { setdatabasename(e.target.value) }} placeholder="Database Name" />
-                                            </div>
-                                            <div className="intro-y col-span-12 flex items-center justify-center sm:justify-end mt-5">
-                                                <button type="submit" className="btn btn-secondary w-24">Create</button>
+                                            <div className="col-span-12 2xl:col-span-2">
+                                                <div className="intro-y  flex items-center justify-center sm:justify-end">
+                                                    <button type="submit" className="btn btn-secondary w-24">Create</button>
+                                                </div>
                                             </div>
                                         </div>
                                     </form>
                                 </div>
-                                <div className="col-span-12 2xl:col-span-6">
+                                <div className="col-span-12 2xl:col-span-12">
                                     <table className="table intro-y table-bordered md:table-fixed  table table-striped">
                                         <thead>
                                             <tr>
                                                 <th className={`whitespace-nowrap`}>Database Name</th>
+                                                <th className={`whitespace-nowrap`} style={{ width: "88px" }}>Tables</th>
                                                 <th className={`whitespace-nowrap`} style={{ width: "88px" }}>Total Table</th>
                                             </tr>
                                         </thead>
@@ -172,11 +215,12 @@ const Settings: NextPage = () => {
                                             {
                                                 (DataBaseList.data || "").map((item: any) => {
                                                     return (
-                                                        <tr>
+                                                        <tr style={{ opacity: `${item.name === "information_schema" ? "0.4" : ""}` }}>
                                                             <td>{item.name}</td>
+                                                            <td>12</td>
                                                             <td className="text-center" style={{ width: "110px" }}>
-                                                                <div id={item.name} onClick={(e) => { HandlerDeleteDatabase(e.currentTarget.id) }} className="pointer-cursor">
-                                                                    <div className="delete flex items-center  cursor-pointer text-danger">
+                                                                <div id={item.name} onClick={(e) => { item.name === "information_schema" ? (HandlerEmptyDatabase) : (HandlerDeleteDatabase(e.currentTarget.id)) }} className={`pointer-cursor`}>
+                                                                    <div className="delete flex items-center  pointer-cursor text-danger">
                                                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" icon-name="trash-2" data-lucide="trash-2" className="lucide lucide-trash-2 w-4 h-4 mr-1">
                                                                             <polyline points="3 6 5 6 21 6"></polyline>
                                                                             <path d="M19 6v14a2 2 0 01-2 2H7a2 2 0 01-2-2V6m3 0V4a2 2 0 012-2h4a2 2 0 012 2v2"></path>
@@ -217,15 +261,12 @@ const Settings: NextPage = () => {
                             <form onSubmit={FormSubmit} method="post">
                                 <div className="font-medium text-base">Create Database 3</div>
                                 <div className="grid grid-cols-12 gap-4 gap-y-5 mt-5">
-
                                     <div className="intro-y col-span-12 sm:col-span-6">
                                         <label htmlFor="input-wizard-1" className="form-label">Database Name</label>
                                     </div>
-
                                     <div className="intro-y col-span-12 flex items-center justify-center sm:justify-end mt-5">
                                         <button type="submit" className="btn btn-secondary w-24">Create</button>
                                     </div>
-
                                 </div>
                             </form>
                         </div>
